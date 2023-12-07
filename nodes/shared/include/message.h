10d23518e89f27a5dd09698b11d11bca6396b737
goncalo <goncalolsilva@ua.pt>
2023-12-07 11:59:27 +0000

    message class

--- nodes/shared/include/message.h
+++ nodes/shared/include/message.h
@@ -2,12 +2,38 @@
 
 #include <Arduino.h>
 
-#define MESSAGE_SIZE
+// in bytes
+#define MESSAGE_TYPE_SIZE 2
+#define MESSAGE_NODE_ID_SIZE 4
+#define MESSAGE_PACKET_ID
+#define MESSAGE_PAYLOAD_SIZE
+#define MESSAGE_TIMESTAMP_SIZE
+#define MAX_MESSAGE_SIZE 
 
-class message {
+enum MESSAGE_TYPE {HELLO, HELLO_RESPONSE, OPENING_REQUEST, OPENING_RESPONSE, OPENING_RESPONSE_ACK}
+
+class Message {
    private:
-    /* data */
+    uint16_t nodeId;
+
    public:
-    message(/* args */);
-    ~message();
+    Message(uint16_t nodeId);
+    ~Message();
+    
+    //! Attention, this function returns a memory allocated pointer, please FREE it!
+    uint8_t* hello(uint32_t& size, uint32_t packetId);
+
+    //! Attention, this function returns a memory allocated pointer, please FREE it!
+    uint8_t* hello_response(uint32_t& size, uint32_t packetId);
+
+    //! Attention, this function returns a memory allocated pointer, please FREE it!
+    uint8_t* open_request(uint16_t userId, uint32_t packetId, long timestamp, uint32_t& size);
+
+    //! Attention, this function returns a memory allocated pointer, please FREE it!
+    uint8_t* open_response(uint32_t& size, uint32_t packetId);
+
+    //! Attention, this function returns a memory allocated pointer, please FREE it!
+    uint8_t* open_response_ack(uint32_t& size, uint32_t packetId);
+
+    uint8_t get_type(uint8_t* message, size_t messageSize);
 };
--- nodes/shared/src/message.cpp
+++ nodes/shared/src/message.cpp
@@ -1,6 +1,42 @@
 #include <message.h>
 
-message::message(/* args */) {}
+Message::Message(uint16_t nodeId) {
+    this->nodeId = nodeId;
+}
 
-message::~message() {}
+Message::~Message() {}
 
+uint8_t* Message::hello(uint32_t& size){
+    size = MESSAGE_NODE_ID_SIZE + MESSAGE_PACKET_ID;
+    uint8_t* message = (uint8_t*) malloc(size);
+    
+    // Place the header in the new message
+    memcpy(message, &messageType, sizeof(messageType));
+    memcpy(message + sizeof(messageType), &photoCount, sizeof(photoCount));
+    memcpy(newMessage + sizeof(messageType) + sizeof(photoCount), &bytesIndex, sizeof(bytesIndex));
+
+    // Place the payload data
+    memcpy(newMessage + headerSize, message, messageSize);
+
+    return newMessage;
+}
+
+uint8_t* Message::hello_response(uint32_t& size){
+
+}
+
+uint8_t* Message::open_request(uint16_t userId, long timestamp, uint32_t& size){
+
+}
+
+uint8_t* Message::open_response(uint32_t& size){
+
+}
+
+uint8_t* Message::open_response_ack(uint32_t& size){
+
+}
+
+uint8_t Message::get_type(uint8_t* message, size_t messageSize){
+
+}
